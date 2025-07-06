38c0c7f733453f92c859cfc632c1c1d01051d5d3
jingjiezhou2021 <musketeerdt@icloud.com>
2025-07-06 16:06:54 +0800

    front-end setup

---
+++ packages/front-end/i18next-parser.config.ts
@@ -0,0 +1,32 @@
+module.exports = {
+	locales: ['en','zh'],
+	defaultNamespace: 'common',
+	defaultValue: (locale: string, namespace: string, key: string) => {
+		if (locale === 'en') {
+			return key;
+		} else {
+			return '';
+		}
+	},
+	output: 'public/locales/$LOCALE/$NAMESPACE.json',
+	input: ['pages/**/*.{js,jsx,ts,tsx}'],
+	createOldCatalogs: false,
+	keepRemoved: false,
+	sort: true,
+	lexers: {
+		tsx: [
+			{
+				lexer: 'JavascriptLexer',
+				functions: ['t'], // Array of functions to match
+				namespaceFunctions: ['useTranslation'], // Array of functions to match for namespace
+			},
+		],
+		ts: [
+			{
+				lexer: 'JavascriptLexer',
+				functions: ['t'], // Array of functions to match
+				namespaceFunctions: ['useTranslation'], // Array of functions to match for namespace
+			},
+		],
+	},
+};
---
+++ packages/front-end/pages/index.tsx
@@ -0,0 +1,17 @@
+import { InferGetStaticPropsType } from "next";
+import { useTranslation } from "next-i18next";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+export async function getStaticProps({ locale }) {
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, ["common"])),
+      // Will be passed to the page component as props
+    },
+  };
+}
+export default function Page(
+    _props: InferGetStaticPropsType<typeof getStaticProps>
+) {
+  const { t } = useTranslation('common')
+  return <h1>{t('Hello, Next.js!')}</h1>;
+}
