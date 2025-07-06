eaf5622f3eec7c18206aa19437d7b1d070d04a12
jingjiezhou2021 <musketeerdt@icloud.com>
2025-07-06 13:27:30 +0800

    smart contracts index file

---
+++ packages/smart-contract/index.ts
@@ -0,0 +1,3 @@
+import NFTMarketPlaceArtifacts from "./artifacts/contracts/NFTMarketPlace.sol/NftMarketplace.json";
+import * as TypeChain from "./typechain-types/index";
+export { NFTMarketPlaceArtifacts,TypeChain };
