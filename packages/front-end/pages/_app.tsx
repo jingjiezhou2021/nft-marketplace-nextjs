93716b37079701b85340815915703470016699b5
jingjiezhou2021 <musketeerdt@icloud.com>
2025-07-06 23:01:49 +0800

    tailwind css

--- packages/front-end/pages/_app.tsx
+++ packages/front-end/pages/_app.tsx
@@ -1,7 +1,7 @@
 import type { AppProps } from 'next/app';
 import { appWithTranslation } from 'next-i18next';
 import RainbowKitAllProvider from '../components/providers/RainbowKitAllProvider';
-
+import '@/public/styles/global.css';
 function App({ Component, pageProps }: AppProps) {
 	return (
 		<RainbowKitAllProvider>
