8de6ac256d1820c82ad8cc214716c92f01ff983d
jingjiezhou2021 <musketeerdt@icloud.com>
2025-07-08 01:55:56 +0800

    apollo provider

--- packages/front-end/pages/_app.tsx
+++ packages/front-end/pages/_app.tsx
@@ -2,11 +2,16 @@ import type { AppProps } from 'next/app';
 import { appWithTranslation } from 'next-i18next';
 import RainbowKitAllProvider from '../components/providers/RainbowKitAllProvider';
 import '@/public/styles/global.css';
+import createApolloClient from '@/apollo';
+import { ApolloProvider } from '@apollo/client';
+const client = createApolloClient();
 function App({ Component, pageProps }: AppProps) {
 	return (
-		<RainbowKitAllProvider>
-			<Component {...pageProps} />
-		</RainbowKitAllProvider>
+		<ApolloProvider client={client}>
+			<RainbowKitAllProvider>
+				<Component {...pageProps} />
+			</RainbowKitAllProvider>
+		</ApolloProvider>
 	);
 }
 
