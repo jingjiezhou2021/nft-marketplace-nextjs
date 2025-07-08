c95e4dde52ae391788854a9a97b072dafd11c3d0
jingjiezhou2021 <musketeerdt@icloud.com>
2025-07-08 16:28:05 +0800

    put sigint hook in sync code

--- packages/monitor-server/graphql/index.ts
+++ packages/monitor-server/graphql/index.ts
@@ -7,66 +7,61 @@ import { ApolloServer } from "@apollo/server";
 import { expressMiddleware } from "@as-integrations/express5";
 import { Prisma, PrismaClient } from "../prisma/generated/prisma";
 import { DefaultArgs } from "../prisma/generated/prisma/runtime/library";
+interface MyContext {
+  prisma: MyPrismaClient;
+}
 type MyPrismaClient = PrismaClient<
   Prisma.PrismaClientOptions,
   never,
   DefaultArgs
 >;
-// interface ContextWithPrisma {
-//   prisma: MyPrismaClient;
-// }
-export async function getApolloServerMiddleware() {
-  async function deleteAllData(prisma: MyPrismaClient) {
-    try {
-      await prisma.nftMarketplace__ItemListed.deleteMany({});
-      await prisma.nftMarketplace__ItemCanceled.deleteMany({});
-      console.log("✅ All data deleted.");
-    } catch (err) {
-      console.error("❌ Failed to delete data:", err);
-    }
+async function deleteAllData(prisma: MyPrismaClient) {
+  try {
+    await prisma.nftMarketplace__ItemListed.deleteMany({});
+    await prisma.nftMarketplace__ItemCanceled.deleteMany({});
+    console.log("✅ All data deleted.");
+  } catch (err) {
+    console.error("❌ Failed to delete data:", err);
   }
-  const prisma = new PrismaClient();
-  const typeDefs = await loadSchema(
-    path.join(process.cwd(), "graphql", "schema.graphql"),
-    {
-      loaders: [new GraphQLFileLoader()],
-    }
-  );
-  const resolvers: Resolvers = {
-    BigInt: BigIntScalar,
-    Query: {
-      itemListedEvents: async (_: any, __: any) => {
-        return prisma.nftMarketplace__ItemListed.findMany();
-      },
-      itemCanceledEvents: async (_: any, __: any) => {
-        return prisma.nftMarketplace__ItemCanceled.findMany();
-      },
-    },
-  };
-  const apolloServer = new ApolloServer({
-    typeDefs,
-    resolvers,
-  });
-  await apolloServer.start();
+}
+export function getApolloServerMiddleware() {
   process.on("SIGINT", async () => {
     console.log("🛑 Shutting down Apollo Server...");
-    try {
-      await new Promise<null>((res) => {
-        setTimeout(() => {
-          console.log("111");
-          res(null);
-        }, 100);
+    await apolloServer.stop();
+    await deleteAllData(prisma);
+    await prisma.$disconnect();
+    process.exit(0);
+  });
+  const prisma = new PrismaClient();
+  let apolloServer: ApolloServer<MyContext>;
+  return loadSchema(path.join(process.cwd(), "graphql", "schema.graphql"), {
+    loaders: [new GraphQLFileLoader()],
+  })
+    .then((typeDefs) => {
+      const resolvers: Resolvers<MyContext> = {
+        BigInt: BigIntScalar,
+        Query: {
+          itemListedEvents: async (_: any, __: any, { prisma }) => {
+            return prisma.nftMarketplace__ItemListed.findMany();
+          },
+          itemCanceledEvents: async (_: any, __: any, { prisma }) => {
+            return prisma.nftMarketplace__ItemCanceled.findMany();
+          },
+        },
+      };
+      apolloServer = new ApolloServer({
+        typeDefs,
+        resolvers,
       });
-    //   await apolloServer.stop();
-    //   await deleteAllData(prisma);
-    //   await prisma.$disconnect();
-    } catch (err) {
-      console.error(err);
-    }
-  });
-  return expressMiddleware(apolloServer, {
-    async context({ req, res }) {
-      return {};
-    },
-  });
+      return apolloServer.start();
+    })
+    .then(() => {
+      return expressMiddleware(apolloServer, {
+        async context({ req, res }) {
+          return {
+            prisma,
+          };
+        },
+      });
+    });
 }
--- packages/monitor-server/src/index.ts
+++ packages/monitor-server/src/index.ts
@@ -2,16 +2,9 @@ import express from "express";
 import cors from "cors";
 import { getApolloServerMiddleware } from "../graphql";
 import { setUpEventListener } from "./utils/events";
-async function main() {
-  await setUpEventListener();
+function main() {
   const app = express();
   const port = process.env.PORT || 3000;
-  app.use(
-    "/graphql",
-    cors<cors.CorsRequest>(),
-    express.json(),
-    await getApolloServerMiddleware()
-  );
   const server = app.listen(port, () => {
     console.log(`🚀 Express server running on http://localhost:${port}`);
   });
@@ -19,12 +12,13 @@ async function main() {
     console.log("🛑 Shutting down Express Server...");
     server.close();
   });
+  setUpEventListener()
+    .then(() => {
+      return getApolloServerMiddleware();
+    })
+    .then((middleware) => {
+      app.use("/graphql", cors(), express.json(), middleware);
+    });
 }
 
 main()
-  .then(() => {
-    console.log("server setup successful");
-  })
-  .catch((err) => {
-    console.error("server setup failed", err);
-  });
