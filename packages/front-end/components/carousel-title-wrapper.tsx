92722794bc7ecf8dfbd53150cc552d9c8ec2befb
jingjiezhou2021 <musketeerdt@icloud.com>
2025-07-15 19:25:03 +0800

    carousel title wrapper

---
+++ packages/front-end/components/carousel-title-wrapper.tsx
@@ -0,0 +1,19 @@
+import { ReactNode } from 'react';
+
+export default function CarouselTitleWrapper({
+	children,
+	title,
+	subtitle,
+}: {
+	children: ReactNode;
+	title: string;
+	subtitle: string;
+}) {
+	return (
+		<div>
+			<h2 className="text-2xl font-bold mb-2 mt-4">{title}</h2>
+			<h5 className="text-muted-foreground text-sm">{subtitle}</h5>
+			{children}
+		</div>
+	);
+}
