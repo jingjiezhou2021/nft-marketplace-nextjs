9027107e20ce25db5953afadfb33d1afa13610db
jingjiezhou2021 <musketeerdt@icloud.com>
2025-08-05 07:07:16 +0800

    tsc do strict null check

--- packages/front-end/eslint.config.js
+++ packages/front-end/eslint.config.js
@@ -17,6 +17,7 @@ const eslintConfig = [
 			'@typescript-eslint/no-unused-vars': 'warn',
 			'@typescript-eslint/no-require-imports': 'warn',
 			'@typescript-eslint/no-explicit-any': 'warn',
+			'@typescript-eslint/no-non-null-asserted-optional-chain': 'warn',
 		},
 	}),
 ];
