e0700dc80d2e93e730b729ee2963a5b16bf777ff
jingjiezhou2021 <musketeerdt@icloud.com>
2025-08-17 10:29:54 +0800

    capsulate steps dialog

--- packages/front-end/components/nft/dialog/list-item.tsx
+++ packages/front-end/components/nft/dialog/list-item.tsx
@@ -1,10 +1,9 @@
-import { LoadingMask, LoadingSpinner } from '@/components/loading';
 import { config } from '@/components/providers/RainbowKitAllProvider';
+import StepsDialog from '@/components/steps-dialog';
 import { Button } from '@/components/ui/button';
 import { Dialog, DialogContent } from '@/components/ui/dialog';
 import MARKETPLACE_ADDRESS from '@/lib/market';
 import { ChainIdParameter } from '@wagmi/core/internal';
-import { Steps } from 'antd';
 import useMessage from 'antd/es/message/useMessage';
 import { CircleCheckIcon } from 'lucide-react';
 import { useTranslation } from 'next-i18next';
@@ -70,126 +69,94 @@ export default function ListItemActionDialog({
 		}
 	}, [authorizeConfirmed, postListingConfirmed]);
 	return (
-		<Dialog {...props}>
-			{contextHolder}
-			<DialogContent
-				showCloseButton={false}
-				onPointerDownOutside={(e) => e.preventDefault()}
-				className="md:max-w-5xl"
-			>
-				<div className="relative flex flex-col gap-6">
-					<LoadingMask
-						className="flex justify-center items-center top-0"
-						loading={
-							authorizeConfirming ||
-							authorizePending ||
-							postListingConfirming ||
-							postListingPending
-						}
-					>
-						<LoadingSpinner size={36} />
-					</LoadingMask>
-					<Steps
-						current={currentStep}
-						items={[
-							{
-								title: t('Authorization'),
-								description: t(
-									'Allow the transfer of this NFT when sold',
-								),
-							},
-							{
-								title: t('Post'),
-								description: t(
-									'Everything is ready, now you can finally post your listing',
-								),
-							},
-						]}
+		<StepsDialog
+			{...props}
+			successContent={
+				<div className="w-full flex flex-col gap-4 items-center">
+					{contextHolder}
+					<CircleCheckIcon
+						className="text-primary"
+						size={36}
 					/>
-					<div className="w-full flex-col items-center gap-4 flex relative">
-						{currentStep !== 2 ? (
-							<>
-								<Button
-									className="flex w-1/2 py-3 h-auto max-w-64"
-									disabled={currentStep !== 0}
-									variant={
-										currentStep === 0
-											? 'default'
-											: 'secondary'
-									}
-									onClick={() => {
-										writeAuthorize({
-											address,
-											chainId,
-											args: [
-												MARKETPLACE_ADDRESS[
-													chainId!
-												] as `0x${string}`,
-												BigInt(tokenId),
-											],
-										});
-									}}
-								>
-									{t('Authorize')}
-								</Button>
-								<Button
-									className="flex w-1/2 py-3 h-auto max-w-64"
-									disabled={currentStep !== 1}
-									variant={
-										currentStep === 1
-											? 'default'
-											: 'secondary'
-									}
-									onClick={() => {
-										if (decimals === undefined) {
-											const errMsg =
-												'Unable to read decimal of the currency';
-											// t(
-											// 	'Unable to read decimal of the currency',
-											// );
-											messageApi.error(t(errMsg));
-											throw new Error(errMsg);
-										}
-										writePostListing({
-											address: MARKETPLACE_ADDRESS[
-												chainId!
-											] as `0x${string}`,
-											chainId,
-											args: [
-												address,
-												BigInt(tokenId),
-												parseUnits(
-													amount.toString(),
-													decimals,
-												),
-												currencyAddress,
-											],
-										});
-									}}
-								>
-									{t('Post')}
-								</Button>
-							</>
-						) : (
-							<div className="w-full flex flex-col gap-4 items-center">
-								<CircleCheckIcon
-									className="text-primary"
-									size={36}
-								/>
-								<h3>{t('Post Listing Item successful')}</h3>
-								<Link
-									href={`/nft/${chainId}/${address}/${tokenId}`}
-									locale={i18n.language}
-								>
-									<Button>
-										{t('Go back to detail page')}
-									</Button>
-								</Link>
-							</div>
-						)}
-					</div>
+					<h3>{t('Post Listing Item successful')}</h3>
+					<Link
+						href={`/nft/${chainId}/${address}/${tokenId}`}
+						locale={i18n.language}
+					>
+						<Button>{t('Go back to detail page')}</Button>
+					</Link>
 				</div>
-			</DialogContent>
-		</Dialog>
+			}
+			loading={
+				authorizeConfirming ||
+				authorizePending ||
+				postListingConfirming ||
+				postListingPending
+			}
+			currentStep={currentStep}
+			steps={[
+				{
+					title: t('Authorization'),
+					description: t('Allow the transfer of this NFT when sold'),
+				},
+				{
+					title: t('Post'),
+					description: t(
+						'Everything is ready, now you can finally post your listing',
+					),
+				},
+			]}
+		>
+			<>
+				{contextHolder}
+				<Button
+					className="flex w-1/2 py-3 h-auto max-w-64"
+					disabled={currentStep !== 0}
+					variant={currentStep === 0 ? 'default' : 'secondary'}
+					onClick={() => {
+						writeAuthorize({
+							address,
+							chainId,
+							args: [
+								MARKETPLACE_ADDRESS[chainId!] as `0x${string}`,
+								BigInt(tokenId),
+							],
+						});
+					}}
+				>
+					{t('Authorize')}
+				</Button>
+				<Button
+					className="flex w-1/2 py-3 h-auto max-w-64"
+					disabled={currentStep !== 1}
+					variant={currentStep === 1 ? 'default' : 'secondary'}
+					onClick={() => {
+						if (decimals === undefined) {
+							const errMsg =
+								'Unable to read decimal of the currency';
+							// t(
+							// 	'Unable to read decimal of the currency',
+							// );
+							messageApi.error(t(errMsg));
+							throw new Error(errMsg);
+						}
+						writePostListing({
+							address: MARKETPLACE_ADDRESS[
+								chainId!
+							] as `0x${string}`,
+							chainId,
+							args: [
+								address,
+								BigInt(tokenId),
+								parseUnits(amount.toString(), decimals),
+								currencyAddress,
+							],
+						});
+					}}
+				>
+					{t('Post')}
+				</Button>
+			</>
+		</StepsDialog>
 	);
 }
---
+++ packages/front-end/components/steps-dialog.tsx
@@ -0,0 +1,48 @@
+import { LoadingMask, LoadingSpinner } from '@/components/loading';
+import { Dialog, DialogContent } from '@/components/ui/dialog';
+import { Steps } from 'antd';
+import { StepProps } from 'antd/lib';
+import { ReactNode } from 'react';
+export default function StepsDialog({
+	loading,
+	currentStep,
+	steps,
+	children,
+	successContent,
+	...props
+}: {
+	loading: boolean;
+	currentStep: number;
+	steps: StepProps[];
+	successContent: ReactNode;
+} & React.ComponentProps<typeof Dialog>) {
+	return (
+		<Dialog {...props}>
+			<DialogContent
+				showCloseButton={false}
+				onPointerDownOutside={(e) => e.preventDefault()}
+				className="md:max-w-5xl"
+			>
+				<div className="relative flex flex-col gap-6">
+					<LoadingMask
+						className="flex justify-center items-center top-0"
+						loading={loading}
+					>
+						<LoadingSpinner size={36} />
+					</LoadingMask>
+					<Steps
+						current={currentStep}
+						items={steps}
+					/>
+					<div className="w-full flex-col items-center gap-4 flex relative">
+						{currentStep !== steps.length ? (
+							<>{children}</>
+						) : (
+							<>{successContent}</>
+						)}
+					</div>
+				</div>
+			</DialogContent>
+		</Dialog>
+	);
+}
