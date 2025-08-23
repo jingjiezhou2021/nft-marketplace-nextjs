4705535ce5abf8f18b857ea5638f43e1e52289fc
jingjiezhou2021 <musketeerdt@icloud.com>
2025-08-23 15:56:34 +0800

    smart contracts export market contract address

--- packages/front-end/lib/market.ts
+++ packages/front-end/lib/market.ts
@@ -1,9 +1,9 @@
-import { base, hardhat, sepolia } from 'viem/chains';
+import { baseSepolia, sepolia } from 'viem/chains';
 import { DeployedAddresses } from 'smart-contract';
 const MARKETPLACE_ADDRESS = {
 	[sepolia.id]:
 		DeployedAddresses[11155111]['NFTMarketPlaceProduction#NftMarketplace'],
-	[hardhat.id]: DeployedAddresses[31337]['NFTMarketPlaceDev#NftMarketplace'],
-	[base.id]: '',
+	[baseSepolia.id]:
+		DeployedAddresses[84532]['NFTMarketPlaceProduction#NftMarketplace'],
 };
 export default MARKETPLACE_ADDRESS;
--- packages/smart-contract/index.ts
+++ packages/smart-contract/index.ts
@@ -2,8 +2,10 @@ import NFTMarketPlaceArtifacts from "./artifacts/contracts/NFTMarketPlace.sol/Nf
 import * as TypeChain from "./typechain-types/index";
 import DeployedAddress31337 from "@/ignition/deployments/chain-31337/deployed_addresses.json";
 import DeployedAddress11155111 from "@/ignition/deployments/chain-11155111/deployed_addresses.json";
+import DeployedAddress84532 from "@/ignition/deployments/chain-84532/deployed_addresses.json";
 const DeployedAddresses = {
   "31337": DeployedAddress31337,
   "11155111": DeployedAddress11155111,
+  "84532": DeployedAddress84532
 };
 export { NFTMarketPlaceArtifacts, TypeChain, DeployedAddresses };
