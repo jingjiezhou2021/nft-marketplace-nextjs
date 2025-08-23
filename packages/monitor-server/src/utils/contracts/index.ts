9df690aaa6bedd10f89f180004ad6ccaa6828d4c
jingjiezhou2021 <musketeerdt@icloud.com>
2025-08-23 15:55:49 +0800

    monitor base with wss endpoint

--- packages/monitor-server/src/utils/contracts/index.ts
+++ packages/monitor-server/src/utils/contracts/index.ts
@@ -4,14 +4,24 @@ import { DeployedAddresses, TypeChain } from "smart-contract";
 export const CHAIN_RPC_URL: Record<number, string> = {
   [31337]: "http://localhost:8545",
   [11155111]: `https://eth-sepolia.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
-  [84532]: `https://base-sepolia.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
+  [84532]: `wss://base-sepolia.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
 };
 export function getProviderOfChain(chainId: number) {
   const rpc = CHAIN_RPC_URL[chainId];
-  const provider = new ethers.JsonRpcProvider(rpc, undefined, {
-    cacheTimeout: -1,
-  });
-  return provider;
+  console.log("chainid:", chainId, "rpc url:", rpc);
+  if (rpc.startsWith("http")) {
+    const provider = new ethers.JsonRpcProvider(rpc, undefined, {
+      cacheTimeout: -1,
+    });
+    return provider;
+  } else if (rpc.startsWith("wss")) {
+    const provider = new ethers.WebSocketProvider(rpc, undefined, {
+      cacheTimeout: -1,
+    });
+    return provider;
+  } else {
+    throw new Error("rpc path unrecognized");
+  }
 }
 export function getMarketContractOfChain(
   chainId: number
@@ -25,6 +35,7 @@ export function getMarketContractOfChain(
       "NFTMarketPlaceProduction#NftMarketplace"
     ];
   if (marketAddress) {
+    console.log("the market address is ", marketAddress);
     const marketContract = TypeChain.NftMarketplace__factory.connect(
       marketAddress,
       provider
