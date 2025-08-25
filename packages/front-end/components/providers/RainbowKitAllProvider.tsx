fc81d712fd9fd27bc3a8477ab55cdcc6a89c6c5a
jingjiezhou2021 <musketeerdt@icloud.com>
2025-08-25 11:11:36 +0800

    front end support base sepolia

--- packages/front-end/components/providers/RainbowKitAllProvider.tsx
+++ packages/front-end/components/providers/RainbowKitAllProvider.tsx
@@ -7,13 +7,13 @@ import {
 	RainbowKitProvider,
 } from '@rainbow-me/rainbowkit';
 import { http, WagmiProvider } from 'wagmi';
-import { mainnet, sepolia, hardhat } from 'wagmi/chains';
+import { mainnet, sepolia, hardhat, baseSepolia } from 'wagmi/chains';
 import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
 import { ReactNode } from 'react';
 import { RainbowKitProviderProps } from '@rainbow-me/rainbowkit/dist/components/RainbowKitProvider/RainbowKitProvider';
 import { useTheme } from 'next-themes';
 import { useRouter } from 'next/router';
-export const chains = [sepolia, hardhat] as const;
+export const chains = [sepolia, baseSepolia] as const;
 export const config = getDefaultConfig({
 	projectId: '51647972c0a9da02ca0e99a6bafefc81',
 	appName: 'NFT Marketplace',
@@ -22,7 +22,9 @@ export const config = getDefaultConfig({
 		[sepolia.id]: http(
 			`https://eth-sepolia.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_ALCHEMY_KEY}`,
 		),
-		[hardhat.id]: http('http://localhost:8545'),
+		[baseSepolia.id]: http(
+			`https://base-sepolia.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_ALCHEMY_KEY}`,
+		),
 	},
 	ssr: true, // If your dApp uses server side rendering (SSR)
 });
--- packages/front-end/lib/chain.tsx
+++ packages/front-end/lib/chain.tsx
@@ -1,15 +1,17 @@
 import {
+	BaseCircleColorful,
 	EthereumCircleColorful,
 	HardhatColorful,
 } from '@ant-design/web3-icons';
 import { ReactElement } from 'react';
-import { hardhat, mainnet, sepolia } from 'viem/chains';
+import { baseSepolia, hardhat, mainnet, sepolia } from 'viem/chains';
 
 export function getIconOfChain(chain: string | number | bigint | undefined) {
 	const map = new Map<number, ReactElement>([
 		[mainnet.id, <EthereumCircleColorful key="ethereum" />],
 		[sepolia.id, <EthereumCircleColorful key="sepolia" />],
 		[hardhat.id, <HardhatColorful key="hardhat" />],
+		[baseSepolia.id, <BaseCircleColorful key="base-sepolia" />],
 	]);
 	return map.get(Number(chain));
 }
@@ -22,6 +24,7 @@ export function getNameOfChain(chainId: string | number | bigint | undefined) {
 	const map = new Map<number, string>([
 		[mainnet.id, 'Ethereum'],
 		[sepolia.id, 'Sepolia'],
+		[baseSepolia.id, 'Base Sepolia'],
 		[hardhat.id, 'Hardhat'],
 	]);
 	return map.get(chainId);
@@ -34,6 +37,7 @@ export function getExplorerOfChain(chainId: string | number | bigint) {
 	chainId = Number(chainId);
 	const map = new Map<number, string>([
 		[mainnet.id, 'https://etherscan.io/address/'],
+		[baseSepolia.id, 'https://sepolia.basescan.org/address/'],
 		[sepolia.id, 'https://sepolia.etherscan.io/address/'],
 	]);
 	return map.get(chainId);
